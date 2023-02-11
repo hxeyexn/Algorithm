f6593cb3272cff0b54c0b27124e7865af42baeaa
hxeyexn <103019852+hxeyexn@users.noreply.github.com>
2023-02-11 12:43:20 +0900

    Update 구구단.java

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int A;
-		A = scanner.nextInt();
+		int A = scanner.nextInt();
 		
 		for(int i = 1; i < 10; i++) {
 			System.out.printf("%d * %d = %d\n", A, i, A*i);
