428b034fbd9a94d38b590f04c3a5776034cc27a4
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2023-10-13 23:03:23 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.kt
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.kt
@@ -2,11 +2,11 @@ class Solution {
     fun solution(n: Int): Int {
         var num = n
         var answer = 0
-    
-        do {
+        
+        while (num != 0) {
             answer += num % 10
             num = num / 10 
-        } while (num != 0)
+        }
         
         return answer
     }
