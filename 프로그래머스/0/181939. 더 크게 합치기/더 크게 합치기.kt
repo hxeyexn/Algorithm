2ccdb4de47651de3be22c3505bf8fd8e415021fe
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2024-11-03 20:38:49 +0900

    [level 0] Title: 더 크게 합치기, Time: 18.60 ms, Memory: 59.9 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.kt
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.kt
@@ -1,8 +1,5 @@
+import kotlin.math.max
+
 class Solution {
-    fun solution(a: Int, b: Int): Int {
-        val s1 = String.format("%d%d", a, b).toInt()
-        val s2 = String.format("%d%d", b, a).toInt()
-        
-        return if (s1 >= s2) s1 else s2
-    }
+    fun solution(a: Int, b: Int): Int = max("$a$b".toInt(), "$b$a".toInt())
 }
