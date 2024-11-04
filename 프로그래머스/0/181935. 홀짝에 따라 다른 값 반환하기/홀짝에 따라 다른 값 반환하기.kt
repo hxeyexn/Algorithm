28e9ad92a3427361f8179d7e19aa64e9fee2b351
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2024-11-04 13:32:36 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 15.04 ms, Memory: 59.7 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.kt
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.kt
@@ -1,15 +1,4 @@
 class Solution {
-    fun solution(n: Int): Int {
-        var answer = 0
-        if (n % 2 == 1) {
-            for (i in 1..n step 2) {
-                answer += i
-            }
-        } else {
-            for (i in 2..n step 2) {
-                answer += i * i
-            }
-        }
-        return answer
-    }
+    fun solution(n: Int): Int = 
+        if (n % 2 == 1) (1..n step 2).sum() else (2..n step 2).sumOf { it * it }
 }
