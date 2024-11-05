8e0863bafd4f03787911e90ffcc14a82630fda68
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2024-11-05 17:49:22 +0900

    [level 0] Title: 코드 처리하기, Time: 418.40 ms, Memory: 358 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.kt
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.kt
@@ -5,14 +5,10 @@ class Solution {
         
         repeat(code.length) { idx ->
             if (mode == 0) {
-                if (code[idx] != '1' && idx % 2 == 0) {
-                    answer += code[idx]
-                }
+                if (code[idx] != '1' && idx % 2 == 0) answer += code[idx]
                 if (code[idx] == '1') mode = 1
             } else {
-                if (code[idx] != '1' && idx % 2 == 1) {
-                    answer += code[idx]
-                } 
+                if (code[idx] != '1' && idx % 2 == 1) answer += code[idx]
                 if (code[idx] == '1') mode = 0
             }
         }
