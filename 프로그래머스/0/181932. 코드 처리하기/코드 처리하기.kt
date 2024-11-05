09344310c15f4ed9576521122ec7df83dc39d103
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2024-11-05 17:38:24 +0900

    [level 0] Title: 코드 처리하기, Time: 402.95 ms, Memory: 358 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun solution(code: String): String {
+        var answer: String = ""
+        var mode: Int = 0
+        
+        repeat(code.length) { idx ->
+            if (mode == 0) {
+                if (code[idx] != '1' && idx % 2 == 0) {
+                    answer += code[idx]
+                }
+                if (code[idx] == '1') mode = 1
+            } else {
+                if (code[idx] != '1' && idx % 2 == 1) {
+                    answer += code[idx]
+                } 
+                if (code[idx] == '1') mode = 0
+            }
+        }
+        return if (answer.isEmpty()) "EMPTY" else answer
+    }
+}
