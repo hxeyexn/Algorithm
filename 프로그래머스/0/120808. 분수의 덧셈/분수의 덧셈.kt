3369d0550d385fff5f7241377e317d6d30fa1c0b
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2024-11-19 20:42:28 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.33 ms, Memory: 61 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.kt
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.kt
@@ -1,15 +1,19 @@
-import kotlin.math.min
+import kotlin.math.*
 
 class Solution {
     fun solution(numer1: Int, denom1: Int, numer2: Int, denom2: Int): IntArray {
         val numer = numer1 * denom2 + numer2 * denom1
-        val denom = denom1 * denom2
-        var max = 1
-        
-        for (i: Int in 2 .. min(numer, denom)) {
-            if (denom % i == 0 && numer % i == 0) max = i
-        }
+        val denom = denom1 * denom2 
+        val gcd = gcd(numer, denom)
     
-        return intArrayOf(numer / max, denom / max)
+        return intArrayOf(numer / gcd, denom / gcd)
     }
+}
+
+fun gcd(numer: Int, denom: Int): Int {
+    val maxValue = max(numer, denom)
+    val minValue = min(numer, denom)
+    var r = maxValue % minValue
+    
+    return if (r == 0) minValue else gcd(minValue, r)
 }
