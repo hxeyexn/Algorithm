4c1e7808248a3898857750a86cbe6d66fca9d592
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2024-11-20 17:46:03 +0900

    [level 0] Title: 세로 읽기, Time: 19.70 ms, Memory: 61.2 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.kt
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.kt
@@ -1,15 +1,4 @@
 class Solution {
-    fun solution(my_string: String, m: Int, c: Int): String {
-        var startIndex = 0
-        var lastIndex = m
-        var answer = ""
-        
-        repeat(my_string.length / m) {
-            answer += my_string.substring(startIndex, lastIndex)[c - 1]
-            startIndex += m
-            lastIndex += m
-        }
-        
-        return answer
-    }
+    fun solution(my_string: String, m: Int, c: Int): String = 
+        my_string.chunked(m).map { it[c - 1] }.joinToString("")
 }
