347203f78b7a2bf436e5360d6b058f8f6a3b5a7f
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2025-02-11 14:43:48 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 116 ms, Memory: 21504 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.kt
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.kt
@@ -1,24 +1,13 @@
 fun main() {
     val elements = 
         readln().split("-").map { element ->
-            if (element == "") 0
-            else element.split("+").sumOf { it.toInt() }
+            element.split("+").sumOf { it.toInt() }
         }
-
-    var sum = 0
     
-    if (elements[0] == 0) {
-        sum += elements[1].unaryMinus()
-        
-        for (idx in 2..< elements.size) {
-            sum += elements[idx].unaryMinus()
-        }
-    } else {
-        sum += elements[0]
-        
-        for (idx in 1..< elements.size) {
-            sum += elements[idx].unaryMinus()
-        }
+    var sum = elements[0]
+    
+    for (idx in 1..< elements.size) {
+        sum -= elements[idx]
     }
     
     print("$sum")
