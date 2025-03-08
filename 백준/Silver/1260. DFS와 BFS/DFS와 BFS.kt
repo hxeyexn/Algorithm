e88e8ea15c69a3d8a5caab340e81c7c8fb75742c
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2025-03-08 22:11:58 +0900

    [Silver II] Title: DFS와 BFS, Time: 284 ms, Memory: 22952 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.kt
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.kt
@@ -1,57 +1,77 @@
-import java.util.LinkedList
-import java.util.Queue
+import java.util.*
 
 fun main() {
+    // 어떤 두 정점 사이에 여러 개의 간선이 있을 수 있음
+    // 입력으로 주어지는 간선은 양방향
     val (n, m, v) = readln().split(" ").map { it.toInt() }
+
+    // 인접 행렬, 인접 리스트로 구현 가능
+    // 정점 번호는 1번부터 N번이기 때문에 n + 1로 계산해 index 값을 별도로 계산 하지 않아도 되도록 함
     val graph = Array(n + 1) { mutableListOf<Int>() }
+
     repeat(m) {
         val (a, b) = readln().split(" ").map { it.toInt() }
-        graph[a].add(b)
+        graph[a].add(b) // 입력으로 주어지는 간선은 양방향
         graph[b].add(a)
     }
 
-    val dfsResult = mutableListOf<Int>()
-    val bfsResult = mutableListOf<Int>()
-    dfs(graph, v, BooleanArray(n + 1) { false }, dfsResult)
-    bfs(graph, v, bfsResult)
+    // 방문할 수 있는 정점이 여러 개인 경우 -> 정점 번호가 작은 것을 먼저 방문
+    graph.map { it.sort() }
 
-    println(dfsResult.joinToString(" "))
-    println(bfsResult.joinToString(" "))
+    dfs(v, graph)
+    bfs(v, graph)
 }
 
-fun dfs(
-    graph: Array<MutableList<Int>>, 
-    v: Int, 
-    visited: BooleanArray, 
-    dfsResult: MutableList<Int>,
-) {
-    visited[v] = true
-    dfsResult.add(v)
-    graph[v].sorted().forEach {
-        if (!visited[it]) {
-            dfs(graph, it, visited, dfsResult)
-        }
-    }
-}
+// dfs: 스택이나 재귀를 활용해 풀이 가능
+fun dfs(start: Int, graph: Array<MutableList<Int>>) {
+    // 각 정점이 "한 번 방문했는지 여부"만 알면 되기 때문에 방문 여부는 1차원 배열 사용
+    val visit = Array(graph.size) { false }
+    val stack = Stack<Int>()
+    val result = mutableListOf<Int>()
 
-fun bfs(
-    graph: Array<MutableList<Int>>, 
-    start: Int, 
-    bfsResult: MutableList<Int>,
-) {
-    val visited = BooleanArray(graph.size) { false }
-    val queue: Queue<Int> = LinkedList()
-    queue.add(start)
-    visited[start] = true
+    stack.push(start)
 
-    while (queue.isNotEmpty()) {
-        val v = queue.remove()
-        bfsResult.add(v)
-        for (i in graph[v].sorted()) {
-            if (!visited[i]) {
-                queue.add(i)
-                visited[i] = true
+    while (stack.isNotEmpty()) {
+        val v = stack.pop()
+
+        // 중복 방문을 방지 하기 위해 pop하는 순간 방문 여부를 체크하고 즉시 처리
+        if (!visit[v]) {
+            visit[v] = true
+            result.add(v)
+
+            // reversed 이유: 스택은 LIFO이기 때문, 정점 번호가 작은 것을 먼저 방문하기 위해서는 스택에 큰 수부터 넣어야 함
+            for (nextV in graph[v].reversed()) {
+                if (!visit[nextV]) {
+                    stack.push(nextV)
+                }
             }
         }
     }
+
+    println(result.joinToString(" "))
+}
+
+// bfs: 큐를 활용해 풀이 가능
+fun bfs(start: Int, graph: Array<MutableList<Int>>) {
+    val visit = Array(graph.size) { false }
+    val queue: Queue<Int> = ArrayDeque()
+    val result = mutableListOf<Int>()
+
+    // 중복 방문을 막기 위해 큐에 넣을 때 즉시 방문 처리
+    queue.add(start)
+    visit[start] = true
+
+    while (queue.isNotEmpty()) {
+        val v = queue.poll()
+        result.add(v)
+
+        for (nextV in graph[v]) {
+            if (!visit[nextV]) {
+                queue.add(nextV)
+                visit[nextV] = true
+            }
+        }
+    }
+
+    println(result.joinToString(" "))
 }
