459a667465b0bb4dc60a838f6d61e613a83dbb7c
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2025-03-19 18:14:07 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1140 ms, Memory: 193596 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.kt
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.kt
@@ -4,19 +4,11 @@ fun main() {
     
     val (n, m) = br.readLine().split(" ").map { it.toInt() }
     
-    val array = Array(n + 1) { IntArray(n + 1) { 0 } }
-    for (i in 1..n) {
-        val input = br.readLine().split(" ").map { it.toInt() }
-        
-        for (j in 1..n) {
-            array[i][j] = input[j - 1]
-        }
-    }
-    
     val sum = Array(n + 1) { IntArray(n + 1) { 0 } }
     for (i in 1..n) {
+        val input = br.readLine().split(" ").map { it.toInt() }
         for (j in 1..n) {
-            sum[i][j] = array[i][j] + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1]
+            sum[i][j] = input[j - 1] + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1]
         }
     }
     
