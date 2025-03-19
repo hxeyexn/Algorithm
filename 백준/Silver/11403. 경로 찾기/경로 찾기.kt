58a5cab48257e30d176b6ed40613b090b11d03bd
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2025-03-20 05:08:28 +0900

    [Silver I] Title: 경로 찾기, Time: 216 ms, Memory: 25116 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.kt
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.kt
@@ -5,11 +5,10 @@ fun main() {
     val result =  Array(n) { IntArray(n) { 0 } }
     
     fun dfs(v: Int, node: Int, visit: BooleanArray) {
-        visit[node] = true
-        result[v][node] = 1
-        
         for (nextNode in 0 ..< n) {
             if (graph[node][nextNode] == 1 && !visit[nextNode]) {
+                visit[nextNode] = true
+                result[v][nextNode] = 1
                 dfs(v, nextNode, visit)
             }
         }
@@ -17,11 +16,7 @@ fun main() {
     
     for (i in 0 ..< n) {
         val visit = BooleanArray(n) { false }
-        for(j in 0 ..< n) {
-            if (graph[i][j] == 1) {
-                dfs(i, j, visit)
-            }
-        }
+        dfs(i, i, visit)
     }
     
     result.forEach {
