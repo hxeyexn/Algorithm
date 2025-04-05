00b3b8aa4c50e7977258f47c7e5c3d6aea0f5c70
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2025-04-06 01:13:56 +0900

    [Bronze II] Title: 다이얼, Time: 76 ms, Memory: 12348 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.kt
+++ 백준/Bronze/5622. 다이얼/다이얼.kt
@@ -1,26 +1,20 @@
 fun main() { 
     val word = readln()
-    val times = mutableMapOf<Char, Int>()
-    var time = 3
-    var count = 0
     var minTime = 0
-    
-    for (key in 'A' .. 'O') {
-        times[key] = time
-        count++
-        if (count == 3) {
-            time++
-            count = 0
-        }
-    }
-    
-    for (key in 'P' .. 'Z') {
-        times[key] = time
-        if (key == 'S' || key == 'V') time++
-    }
-    
+   
     for (w in word) {
-        minTime += times[w] ?: 0
+        minTime += 
+            when (w) {
+                in 'A' .. 'C' -> 3
+                in 'D' .. 'F' -> 4
+                in 'G' .. 'I' -> 5
+                in 'J' .. 'L' -> 6
+                in 'M' .. 'O' -> 7
+                in 'P' .. 'S' -> 8
+                in 'T' .. 'V' -> 9
+                in 'W' .. 'Z' -> 10
+                else -> 0
+            }
     }
     
     print("$minTime")
