3b5b4db5447064daace707ad0c2aae1f98471f73
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2025-04-06 20:21:50 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 704 ms, Memory: 90260 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.kt
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.kt
@@ -1,17 +1,19 @@
 fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
-    val (n, m) = br.readLine().split(" ").map { it.toInt() }
-    val numbers = br.readLine().split(" ").map { it.toInt() }
-    val sum = IntArray(n + 1) { 0 }
     
-    for (i in 1 .. n) {
-        sum[i] = sum[i - 1] + numbers[i - 1]
+    val (N, M) = br.readLine().split(" ").map { it.toInt() }
+    val input = br.readLine().split(" ").map { it.toInt() }
+    val prefixSum = IntArray(N + 1) { 0 }
+    
+    for (i in 1 .. N) {
+        prefixSum[i] = prefixSum[i - 1] + input[i - 1]
     }
     
-    repeat(m) {
+    repeat(M) {
         val (i, j) = br.readLine().split(" ").map { it.toInt() }
-        bw.write("${sum[j] - sum[i - 1]}\n")
+        val sum = prefixSum[j] - prefixSum[i - 1] 
+        bw.write("$sum\n")
     }
     
     bw.flush()
