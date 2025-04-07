c9d40c267f35a2a45fac0cf8383f07854ac95c18
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2025-04-07 17:53:17 +0900

    [Silver V] Title: 수들의 합 5, Time: 128 ms, Memory: 12416 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.kt
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.kt
@@ -1,24 +1,19 @@
 fun main() {
     val N = readln().toInt()
-    val prefixSum = IntArray(N + 1) { 0 }
-    
-    for (i in 1 .. N) {
-        prefixSum[i] = prefixSum[i - 1] + i
-    }
     
     var count = 0
-    var start = 0
+    var sum = 1
+    var start = 1
     var end = 1
     
     while (end <= N) {
-        val sum = prefixSum[end] - prefixSum[start]
         when {
-            sum < N -> end++
+            sum < N -> sum += ++end
             sum == N -> {
                 count++
-                start++
+                sum -= start++
             }
-            sum > N -> start++
+            sum > N -> sum -= start++
         }
     }
     
