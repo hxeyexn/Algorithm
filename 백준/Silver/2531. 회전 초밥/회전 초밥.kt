3b38ad647c7736fb7f436a16555c1b29445006a2
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2025-04-08 19:53:07 +0900

    [Silver I] Title: 회전 초밥, Time: 168 ms, Memory: 24768 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.kt
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.kt
@@ -4,43 +4,39 @@ fun main() {
     val (N, d, k, c) = br.readLine().split(" ").map { it.toInt() }
     val belt = IntArray(N) { br.readLine().toInt() }
     
-    var start = 0
-    var end = k - 1
-    var max = 0
+    // 초밥 번호별로 윈도우 내 등장 횟수를 저장할 배열
+    val count = IntArray(d + 1) { 0 }
     
-    while (start <= N - 1) {
-        var count = 0
-        val visit = BooleanArray(3001) { false }
-        
-        if (start < end) {
-            for (i in start .. end) {
-                if (!visit[belt[i]]) {
-                    count++
-                    visit[belt[i]] = true
-                }
-            }
-        } else {
-            for (i in start ..< N) {
-                if (!visit[belt[i]]) {
-                    count++
-                    visit[belt[i]] = true
-                }
-            }
-            for (i in 0 .. end) {
-                if (!visit[belt[i]]) {
-                    count++
-                    visit[belt[i]] = true
-                }
-            }
-        }
-
-        if (!visit[c]) count++
-        if (max < count) max = count
-        
-        start++
-        if (end == N - 1) end = 0
-        else end++
+    // 초밥 종류 수
+    var kind = 0
+    
+    // 초기 윈도우 설정
+    for (i in 0 ..< k) {
+        if (count[belt[i]] == 0) kind++
+        count[belt[i]]++
     }
     
-    println(max)
+    // max 설정, 쿠폰 체크
+    var max = if (count[c] == 0) kind + 1 else kind
+    
+    // 슬라이딩 윈도우 시작
+    for (i in 1 ..< N) {
+        // 제거할 초밥
+        val remove = belt[(i - 1) % N]
+        count[remove]--
+        if (count[remove] == 0) kind--
+        
+        // 추가할 초밥
+        val add = belt[(i + k - 1) % N]
+        if (count[add] == 0) kind++
+        count[add]++
+        
+        // 쿠폰 재확인
+        val current = if (count[c] == 0) kind + 1 else kind
+        
+        // max 값 설정
+        if (max < current) max = current
+    }
+    
+    print(max)
 }
