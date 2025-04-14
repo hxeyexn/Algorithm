d2a0e65295b3ff66f6dbffd8ce04ebe8d4f58b2d
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2025-04-14 19:44:23 +0900

    [Silver V] Title: 단어 정렬, Time: 352 ms, Memory: 39780 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.kt
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.kt
@@ -1,21 +1,22 @@
 fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
-    
+
     val N = br.readLine().toInt()
     val words = mutableListOf<String>()
-    
+
     repeat(N) {
         words.add(br.readLine())
     }
-    
+
     words.sort()
-    val sortedWords = words.sortedBy { it.length }.toSet()
-    
+    words.sortBy { it.length }
+    val sortedWords = words.toSet()
+
     for (word in sortedWords) {
         bw.write("${word}\n")
     }
-    
+
     bw.flush()
     bw.close()
 }
