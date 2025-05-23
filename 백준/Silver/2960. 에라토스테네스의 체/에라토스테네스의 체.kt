580d29fc808d444c726e0ac684053b1a67d77c65
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2025-05-23 15:13:40 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 156 ms, Memory: 21484 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.kt
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.kt
@@ -1,22 +1,23 @@
 fun main() {
     val (N, K) = readln().split(" ").map { it.toInt() }
     val numbers = (2 .. N).toMutableList()
-
     var count = 0
 
     while (numbers.isNotEmpty()) {
-        // 가장 작은 수 P를 찾는다.
         val P = numbers.min()
+        val iterator = numbers.iterator()
 
-        // P의 배수를 순서대로 지우고 지울 때마다 count를 증가 시킨다.
-        while (numbers.count { it % P == 0 } != 0) {
-            val element = numbers.first { it % P == 0 }
-            numbers.remove(element)
-            count++
+        while (iterator.hasNext()) {
+            val num = iterator.next()
+            
+            if (num % P == 0) {
+                iterator.remove()
+                count++
 
-            if (count == K) {
-                print(element)
-                return
+                if (count == K) {
+                    print(num)
+                    return
+                }
             }
         }
     }
