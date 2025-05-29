f323766b935bafb13c7d2dc9e975ba8dbb061bc7
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2025-05-30 04:15:03 +0900

    [Silver III] Title: 숫자 정사각형, Time: 120 ms, Memory: 21424 KB -BaekjoonHub

--- 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.kt
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.kt
@@ -7,16 +7,15 @@ fun main() {
     val board = Array(N) { br.readLine().map { it.digitToInt() }.toIntArray() }
     var maxD = 0
     
-    for (i in 0 ..< N) {
-        for (j in 0 ..< M) {
+    for (i in 0 until N) {
+        for (j in 0 until M) {
             var d = 0
             
-            while (d < min(N, M)) {
-                val dx = i + d
-                val dy = j + d
-                if (dx > N - 1 || dy > M - 1) break
-                if (board[i][j] == board[i][dy] && board[i][j] == board[dx][j] && board[i][j] == board[dx][dy]) {
-                    if (maxD < d) maxD = d
+            while (i + d < N && j + d < M) {
+                if (board[i][j] == board[i + d][j] 
+                    && board[i][j] == board[i][j + d] 
+                    && board[i][j] == board[i + d][j + d]) {
+                    if (maxD <= d) maxD = d
                 }
                 d++
             }
