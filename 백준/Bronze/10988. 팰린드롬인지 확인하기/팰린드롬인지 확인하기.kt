b6183288fca95a6667587ead3de38b9f82b88201
Hyeyeon Gong <kck85075@naver.com>
2025-07-15 03:20:33 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 76 ms, Memory: 12336 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.kt
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.kt
@@ -3,15 +3,14 @@ import java.util.Stack
 fun main() {
     val word = readln()
     val stack = Stack<Char>()
-    var result = 0
+    var result = 1
     
     word.forEach {
         stack.push(it)
     }
     
-    for (i in 0 until word.length) {
-        if (word[i] == stack.pop()) result = 1
-        else {
+    for (i in 0 until word.length / 2) {
+        if (word[i] != stack.pop()) {
             result = 0
             break
         }
