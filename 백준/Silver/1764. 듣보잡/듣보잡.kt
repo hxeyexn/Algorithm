e595f3f29ab4172ff96d959aee310f451e3fb681
Hyeyeon Gong <kck85075@naver.com>
2025-07-15 20:01:08 +0900

    [Silver IV] Title: 듣보잡, Time: 388 ms, Memory: 42836 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.kt
+++ 백준/Silver/1764. 듣보잡/듣보잡.kt
@@ -1,33 +1,29 @@
-// 듣도 못한 사람의 명단에는 중복되는 이름이 없음
-// 보도 못한 사람의 명단도 마찬가지
-// 듣보잡의 수와 그 명단을 사전순으로 출력한다.
+// 교집합으로 풀기
 fun main() {
-    val (N, M) = readln().split(" ").map { it.toInt() }
     val br = System.`in`.bufferedReader()
-    
-    val map = mutableMapOf<String, Pair<Boolean, Boolean>>()
-    
-    // N 만큼 순회하면서 map에 넣는다. 이때 pair first 값 업데이트
-    // M 만큼 순회하면서 map에 넣는다. 이때 pair second 값 업데이트
+    val bw = System.out.bufferedWriter()
+    val (N, M) = br.readLine().split(" ").map { it.toInt() }
+
+    val unhidden = mutableSetOf<String>()
+    val unseen = mutableSetOf<String>()
     
     repeat(N) {
         val name = br.readLine()
-        map[name] = true to false
+        unhidden.add(name)
     }
     
     repeat(M) {
         val name = br.readLine()
-        val hear = map[name]?.first ?: false
-        map[name] = hear to true
+        unseen.add(name)
     }
     
-    val result = 
-        map.filter { it.value.first && it.value.second }
-            .toSortedMap()
+    val result = unhidden.intersect(unseen).sorted()
     
-    println(result.size)
-    
-    result.keys.forEach {
-        println(it)
+    bw.write("${result.size}\n")
+    result.forEach {
+        bw.write("${it}\n")
     }
+    
+    bw.flush()
+    bw.close()
 }
