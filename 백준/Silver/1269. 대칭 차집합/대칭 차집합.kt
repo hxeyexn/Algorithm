fa6a6a369fc242f244bbcc459075c312515672d6
Hyeyeon Gong <kck85075@naver.com>
2025-07-16 18:48:24 +0900

    [Silver IV] Title: 대칭 차집합, Time: 896 ms, Memory: 134276 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.kt
@@ -0,0 +1,17 @@
+// 자연수를 원소로 갖는 두 집합 A, B
+// A - B / B - A의 합집합을 A와 B의 대칭 차집합
+// 두 집합의 대칭 차집합의 원소의 개수를 출력
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val (a, b) = br.readLine().split(" ").map { it.toInt() }
+    
+    val A = br.readLine().split(" ").toSet()
+    val B = br.readLine().split(" ").toSet()
+    
+    val newA = A.subtract(B)
+    val newB = B.subtract(A)
+    
+    val newSet = newA + newB
+    print("${newSet.size}")
+}
