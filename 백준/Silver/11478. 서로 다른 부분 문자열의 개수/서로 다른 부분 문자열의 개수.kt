fdfaa7f5640e2bf4dcce0637cb74f6c84f52eef5
Hyeyeon Gong <kck85075@naver.com>
2025-07-16 19:51:51 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 484 ms, Memory: 292924 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.kt
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.kt
@@ -3,6 +3,7 @@ fun main() {
     val S = br.readLine()
 
     val set = hashSetOf<String>()
+    var count = 0
     
     for (i in S.length downTo 1) {
         var start = 0
@@ -11,7 +12,10 @@ fun main() {
         repeat(i) {
             set.add(S.substring(start++, end++))
         }
+        
+        count += set.size
+        set.clear()
     }
     
-    print("${set.size}")
+    print("${count}")
 }
