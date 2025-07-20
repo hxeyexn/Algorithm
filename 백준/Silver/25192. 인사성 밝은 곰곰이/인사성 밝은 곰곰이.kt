be3ed2018a7304139eb24018173d84121f475e0a
Hyeyeon Gong <kck85075@naver.com>
2025-07-20 22:23:31 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 252 ms, Memory: 25040 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.kt
@@ -0,0 +1,23 @@
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val N = br.readLine().toInt()
+    val gomgom = HashSet<String>()
+    var count = 0
+    
+    repeat(N) {
+        val input = br.readLine()
+        
+        if (input == "ENTER") {
+            if (it != 0) {
+                count += gomgom.size
+                gomgom.clear()
+            } 
+        } else {
+            gomgom.add(input)
+        }
+    }
+    
+    count += gomgom.size
+    
+    print("$count")
+}
