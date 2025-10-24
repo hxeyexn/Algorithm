93088d0a1d025d331a3e8da58d90d3fdbe19faa7
Hyeyeon Gong <kck85075@naver.com>
2025-10-24 16:53:57 +0900

    [Bronze II] Title: 음계, Time: 72 ms, Memory: 12340 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.kt
+++ 백준/Bronze/2920. 음계/음계.kt
@@ -1,9 +1,16 @@
 fun main() {
-    val input = readln().filterNot { it == ' ' }
+    // 입력을 읽는다
+    val performance = readln().filterNot { it == ' ' }
+    // val performance = readln().replace(" ", "")
     
-    when (input) {
-        "12345678" -> print("ascending")
-        "87654321" -> print("descending")
+    // ascending / descending을 가지고 있는다.
+    val ascending = "12345678"
+    val descending = "87654321"
+    
+    // 값을 비교한다
+    when(performance) {
+        ascending -> print("ascending")
+        descending -> print("descending")
         else -> print("mixed")
     }
 }
