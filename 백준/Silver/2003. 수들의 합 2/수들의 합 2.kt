a6d7be0c0ee9850d8b661abe115fbce95127915c
Hyeyeon Gong <kck85075@naver.com>
2025-10-28 16:07:34 +0900

    [Silver IV] Title: 수들의 합 2, Time: 156 ms, Memory: 22056 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.kt
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.kt
@@ -1,29 +1,25 @@
 fun main() {
     val br = System.`in`.bufferedReader()
     val (N, M) = br.readLine().split(" ").map { it.toInt() }
-    val numbers = br.readLine().split(" ").map { it.toInt() }
-    val prefixSum = IntArray(N + 1) { 0 }
+    val progression = br.readLine().split(" ").map { it.toInt() }.toTypedArray()
+    var (start, end) = 0 to 0
+    var sum = progression[0]
+    var count = 0
     
-    for (i in 1 .. N) {
-        prefixSum[i] = prefixSum[i - 1] + numbers[i - 1]
-    }
-    
-    var start = 0
-    var end = 1
-    var count = 0 
-    
-    do {
-        val sum = prefixSum[end] - prefixSum[start]
+    while (true) {
+        // 현재 부분합이 M과 같으면 count 증가
+        if (sum == M) count++
         
-        when {
-            sum < M -> end++
-            sum == M -> {
-                count++
-                start++
-            }
-            else -> start++
+        // 현재 부분 합이 크거나 같다면 start를 1 증가
+        // 현재 부분 합이 작다면 end를 1 증가
+        if (sum >= M) {
+            sum -= progression[start++]
+        } else {
+            end++
+            if (end == N) break
+            sum += progression[end]
         }
-    } while (end <= N)
+    }
     
     print(count)
 }
