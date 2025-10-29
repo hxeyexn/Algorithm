bc02d3c80dde5984ec665c9255da4b6cccd54303
Hyeyeon Gong <kck85075@naver.com>
2025-10-29 18:57:24 +0900

    [Silver III] Title: N과 M (1), Time: 324 ms, Memory: 45752 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.kt
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.kt
@@ -1,16 +1,13 @@
+import java.io.BufferedWriter
+
 fun main() {
     val (N, M) = readln().split(" ").map { it.toInt() }
     val bw = System.out.bufferedWriter()
     
     val visited = BooleanArray(N + 1) { false }
     val progression = mutableListOf<Int>()
-    val result = mutableListOf<MutableList<Int>>()
     
-    backtracking(N, M, count = 0, visited, progression, result)
-    
-    for (r in result) {
-        bw.write("${r.joinToString(" ")}\n")
-    }
+    backtracking(N, M, count = 0, visited, progression, bw)
     
     bw.flush()
     bw.close()
@@ -22,11 +19,11 @@ fun backtracking(
     count: Int, 
     visited: BooleanArray,
     progression: MutableList<Int>,
-    result: MutableList<MutableList<Int>>,
+    bw: BufferedWriter,
 ) {
     if (count == m) {
         // 수열이 나중에 수정되어도 result에 담긴 리스트는 그 시점의 상태 그대로 유지되야 함
-        result.add(progression.toMutableList())
+        bw.write("${progression.joinToString(" ")}\n")
         return
     }
     
@@ -41,7 +38,7 @@ fun backtracking(
                 count = count + 1, 
                 visited, 
                 progression,
-                result,
+                bw,
             )
             
             visited[i] = false
