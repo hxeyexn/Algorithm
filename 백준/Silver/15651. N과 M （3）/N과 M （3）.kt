ff75d80c2ec3888cb8f9d1915de1b7fe0b4034f1
Hyeyeon Gong <kck85075@naver.com>
2025-10-30 01:44:30 +0900

    [Silver III] Title: N과 M (3), Time: 968 ms, Memory: 266636 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.kt
@@ -0,0 +1,33 @@
+import java.io.BufferedWriter
+
+fun main() {
+    val (N, M) = readln().split(" ").map { it.toInt() }
+    val bw = System.out.bufferedWriter()
+    val progression = mutableListOf<Int>()
+    backtracking(N, M, count = 0, progression, bw)
+    
+    bw.flush()
+    bw.close()
+}
+
+fun backtracking(
+    n: Int, 
+    m: Int,
+    count: Int,
+    progression: MutableList<Int>,
+    bw: BufferedWriter,
+) {
+    // 종료 조건 
+    if (progression.size == m) {
+        // 수열 출력
+        bw.write(progression.joinToString(" "))
+        bw.newLine()
+        return 
+    }
+    
+    for (i in 1 .. n) {
+        progression.add(i)
+        backtracking(n, m, count + 1, progression, bw)
+        progression.removeLast()
+    }
+}
