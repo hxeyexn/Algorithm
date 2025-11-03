fbaad57f6b7136b498eca70e149df0ee4227aac9
Hyeyeon Gong <kck85075@naver.com>
2025-11-04 03:29:15 +0900

    [Silver I] Title: 단지번호붙이기, Time: 108 ms, Memory: 14628 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.kt
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.kt
@@ -1,55 +1,63 @@
-import java.util.Queue
 import java.util.LinkedList
 
 fun main() {
     val br = System.`in`.bufferedReader()
     val N = br.readLine().toInt()
+    val map = Array(N) { IntArray(N) { 0 } }
     
-    // 지도 배열
-    val map = Array(N) { br.readLine().map { it.digitToInt() }.toIntArray() }
-    
-    // 방문 배열
     val visited = Array(N) { BooleanArray(N) { false } }
     
-    // 단지 수
-    var count = 0
+    val dx = intArrayOf(1, 0, -1, 0)
+    val dy = intArrayOf(0, 1, 0, -1)
     
-    // 각 단지에 속하는 집의 수
-    val homes = mutableListOf<Int>()
+    var totalCount = 0
+    val result = mutableListOf<Int>()
     
-    // dx, dy
-    val dx = listOf(1, 0, -1, 0)
-    val dy = listOf(0, 1, 0, -1)
+    for (i in 0 until N) {
+        val row = br.readLine()
+        
+        for (j in 0 until N) {
+            map[i][j] = row[j].digitToInt()
+        }
+    }
     
-    for (i in 0 ..< N) {
-        for (j in 0 ..< N) {
-            val queue: Queue<Pair<Int, Int>> = LinkedList()
+    for (i in 0 until N) {
+        for (j in 0 until N) {
+            if (map[i][j] == 0 || visited[i][j]) continue
             
-            if (visited[i][j] || map[i][j] == 0) continue
+            val queue = LinkedList<Pair<Int, Int>>()
+            var homeCount = 0
+            
             queue.add(i to j)
             visited[i][j] = true
-            count++
+            totalCount++
             
-            var homeCount = 0
-            while (queue.isNotEmpty()) {
-                val (currentX, currentY) = queue.poll()
+            while(queue.isNotEmpty()) {
+                val (x, y) = queue.remove()
                 homeCount++
                 
-                for (k in 0 .. 3) {
-                    val nx = currentX + dx[k]
-                    val ny = currentY + dy[k]
+                for (n in 0 .. 3) {
+                    val nx = x + dx[n]
+                    val ny = y + dy[n]
                     
                     if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue
-                    if (visited[nx][ny] || map[nx][ny] == 0) continue
+                    if (map[nx][ny] == 0 || visited[nx][ny]) continue
+                    
                     queue.add(nx to ny)
                     visited[nx][ny] = true
                 }
             }
-            homes.add(homeCount)
+            
+            result.add(homeCount)
         }
     }
     
-    homes.sort()
-    println(count)
-    homes.forEach { println(it) }
+    // 총 단지 수
+    println("$totalCount")
+    
+    // 각 단지내 집의 수를 오름차순으로 정렬하여 한 줄에 하나씩 출력
+    result.sort()
+    for (count in result) {
+        println("$count")
+    }
 }
