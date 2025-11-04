b955b8c5df337b55178c46c902ad8c791a73563b
Hyeyeon Gong <kck85075@naver.com>
2025-11-04 18:05:37 +0900

    [Gold IV] Title: 부분합, Time: 232 ms, Memory: 30628 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.kt
+++ 백준/Gold/1806. 부분합/부분합.kt
@@ -1,42 +1,41 @@
+import java.util.StringTokenizer
 import kotlin.math.min
-import kotlin.math.abs
 
 fun main() {
     val br = System.`in`.bufferedReader()
-    // N(수열의 길이)과 S(합)를 읽는다
     val (N, S) = br.readLine().split(" ").map { it.toInt() }
-
-    // 수열을 읽는다(공백 제거)
-    val progression = br.readLine().split(" ").map { it.toInt() }
     
-    // 투포인터를 활용한다
-    // start와 end는 0으로 시작한다
-    var (start, end) = 0 to 0
+    val input = StringTokenizer(br.readLine())
+    val progression = IntArray(N + 1) { 0 }
     
-    // 현재 합은 sum으로 정의하며 0으로 초기화한다
-    var sum = 0
+    for (i in 1 .. N) {
+        progression[i] = input.nextToken().toInt()
+    }
     
-    // 최소의 길이(min)를 구해야 하고, 불가능하면 0을 출력
+    var prefixSum = progression[1]
     var minLength = Int.MAX_VALUE
-    var currentLength = 0
     
-    // while 문 사용 -> 종료 조건: end == N
-    while(true) {
-        // 합이 S 이상이면 현재 길이와 min을 비교해 갱신
-        if (sum >= S) minLength = min(minLength, currentLength)
-        
-        // 크거나 같으면 start를 1 증가, sum 갱신
-        // 작으면 end를 1 증가, sum 갱신
-        if (sum >= S) {
-            sum -= progression[start++]
-        } else {
+    var start = 1
+    var end = 1
+    
+    while (true) {
+        if (prefixSum < S) {
             if (end == N) break
-            sum += progression[end++]
+            
+            end++
+            prefixSum += progression[end]
+            continue
         }
         
-        currentLength = abs(start - end)
+        prefixSum -= progression[start]
+        minLength = min(minLength, end - start + 1)
+        start++
     }
-
-    if (minLength != Int.MAX_VALUE) print(minLength)
-    else print(0)
+    
+    if (minLength == Int.MAX_VALUE) {
+        print("0")
+        return
+    }
+    
+    print(minLength)
 }
