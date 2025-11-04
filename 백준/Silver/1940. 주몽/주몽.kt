5c5d5dfac939392ddedf1418573248aabe0d9a38
Hyeyeon Gong <kck85075@naver.com>
2025-11-04 22:34:07 +0900

    [Silver IV] Title: 주몽, Time: 160 ms, Memory: 20004 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.kt
+++ 백준/Silver/1940. 주몽/주몽.kt
@@ -1,17 +1,31 @@
+import java.util.StringTokenizer
+
 fun main() {
     val br = System.`in`.bufferedReader()
     val N = br.readLine().toInt()
     val M = br.readLine().toInt()
-    val materials = br.readLine().split(" ").map { it.toInt() }
+    
+    val input = StringTokenizer(br.readLine())
+    val materials = IntArray(N) { input.nextToken().toInt() }
+    materials.sort()
+    
+    var start = 0
+    var end = N - 1
     var count = 0
 
-    for (start in 0 ..< N - 1) {
-        var end = start + 1
-        while (end < N) {
-            val sum = materials[start] + materials[end++]
-            if (sum == M) count++
+    while (start < end) {
+        val sum = materials[start] + materials[end]
+        
+        when {
+            sum < M -> start++
+            sum > M -> end--
+            else -> {
+                start++
+                end--
+                count++
+            }
         }
-    }
-
-    print(count)
+    } 
+    
+    print("$count")
 }
