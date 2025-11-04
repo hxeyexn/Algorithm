6da98da17bc2f5953fa536ef6a22cd5215d090b0
Hyeyeon Gong <kck85075@naver.com>
2025-11-04 15:38:33 +0900

    [Gold V] Title: 수 고르기, Time: 292 ms, Memory: 33224 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.kt
+++ 백준/Gold/2230. 수 고르기/수 고르기.kt
@@ -1,32 +1,31 @@
+import kotlin.math.abs
 import kotlin.math.min
 
 fun main() {
     val br = System.`in`.bufferedReader()
     val (N, M) = br.readLine().split(" ").map { it.toInt() }
+    
     val progression = IntArray(N) { br.readLine().toInt() }
     progression.sort()
-
-    var start = 0
-    var end = 1
-    var min = Int.MAX_VALUE
     
-    // start와 end가 같아진다면? end를 증가시켜야 함
-    // end가 N에 닿았을 때 start가 움직일 수도 있음
+    var start = 0
+    var end = 0
+    var minSub = Int.MAX_VALUE
+    
     while (end < N) {
         if (start == end) {
             end++
             continue
         }
         
-        val diff = progression[end] - progression[start]
+        val sub = abs(progression[start] - progression[end])
         
-        if (diff >= M) {
-            min = min(diff, min)
+        if (sub < M) end++
+        else {
             start++
-        } else {
-            end++
+            minSub = min(minSub, sub)
         }
     }
     
-    if (min != Int.MAX_VALUE) print(min)
+    print(minSub)
 }
