5078f09a56b1e7bbc336395d32d87d352a19345e
Hyeyeon Gong <kck85075@naver.com>
2025-11-04 16:45:18 +0900

    [Silver III] Title: 수열, Time: 248 ms, Memory: 31796 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.kt
+++ 백준/Silver/2559. 수열/수열.kt
@@ -1,21 +1,31 @@
+import java.util.StringTokenizer
+import kotlin.math.max
+
 fun main() {
     val br = System.`in`.bufferedReader()
     val (N, K) = br.readLine().split(" ").map { it.toInt() }
-    val temp = br.readLine().split(" ").map { it.toInt() }
+    val progression = IntArray(N) { 0 }
+    var maxSum = 0
+    var currentSum = 0
+    
+    val input = StringTokenizer(br.readLine())
+    repeat(N) {
+        progression[it] = input.nextToken().toInt()
+        
+        if (it < K) currentSum += progression[it]
+    }
+    maxSum = currentSum
     
     var start = 1
     var end = K
-    var prefixSum = (0 until K).sumOf { temp[it] }
-    var max = prefixSum
     
     while (end < N) {
-        val sum = prefixSum - temp[start - 1] + temp[end]
-        prefixSum = sum
+        currentSum = currentSum - progression[start - 1] + progression[end]
+        maxSum = max(maxSum, currentSum)
         
-        if (max < sum) max = sum
         start++
         end++
     }
     
-    print(max)
+    print(maxSum)
 }
