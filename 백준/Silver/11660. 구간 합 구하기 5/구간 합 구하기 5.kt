8cbed032451e81762916ca9265ed520ec86b6ca2
Hyeyeon Gong <kck85075@naver.com>
2025-11-05 17:51:47 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1064 ms, Memory: 158648 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.kt
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.kt
@@ -1,21 +1,27 @@
+import java.util.StringTokenizer
+
 fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
+    val (N, M) = br.readLine().split(" ").map { it.toInt() }
     
-    val (n, m) = br.readLine().split(" ").map { it.toInt() }
+    val numbers = Array(N + 1) { IntArray(N + 1) { 0 } }
+    val prefixSum = Array(N + 1) { IntArray(N + 1) { 0 } }
     
-    val sum = Array(n + 1) { IntArray(n + 1) { 0 } }
-    for (i in 1..n) {
-        val input = br.readLine().split(" ").map { it.toInt() }
-        for (j in 1..n) {
-            sum[i][j] = input[j - 1] + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1]
+    for (i in 1 .. N) {
+        val input = StringTokenizer(br.readLine())
+        
+        for (j in 1 .. N) {
+            numbers[i][j] = input.nextToken().toInt()
+            prefixSum[i][j] = numbers[i][j] + prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1]
         }
     }
     
-    repeat(m) {
+    repeat(M) {
         val (x1, y1, x2, y2) = br.readLine().split(" ").map { it.toInt() }
-        val prefixSum = sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1]
-        bw.write("${prefixSum}\n")
+        val sum = prefixSum[x2][y2] - prefixSum[x2][y1 - 1] - prefixSum[x1 - 1][y2] + prefixSum[x1 - 1][y1 - 1]
+        bw.write("$sum")
+        bw.newLine()
     }
     
     bw.flush()
