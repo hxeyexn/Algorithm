b240a4fe1c11bfbfaccd930bb7474f3a9b177ca7
Hyeyeon Gong <kck85075@naver.com>
2025-11-05 15:15:22 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 684 ms, Memory: 84904 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.kt
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.kt
@@ -1,19 +1,26 @@
+import java.util.StringTokenizer
+
 fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
     
     val (N, M) = br.readLine().split(" ").map { it.toInt() }
-    val input = br.readLine().split(" ").map { it.toInt() }
+    val st = StringTokenizer(br.readLine())
+    
+    val numbers = IntArray(N + 1) { 0 }
     val prefixSum = IntArray(N + 1) { 0 }
     
     for (i in 1 .. N) {
-        prefixSum[i] = prefixSum[i - 1] + input[i - 1]
+        numbers[i] = st.nextToken().toInt()
+        prefixSum[i] = numbers[i] + prefixSum[i - 1]
     }
     
     repeat(M) {
         val (i, j) = br.readLine().split(" ").map { it.toInt() }
-        val sum = prefixSum[j] - prefixSum[i - 1] 
-        bw.write("$sum\n")
+        
+        val sum = prefixSum[j] - prefixSum[i - 1]
+        bw.write("$sum")
+        bw.newLine()
     }
     
     bw.flush()
