93e89ea9a653f60f5b6b5635ab03c3ba085dbb34
Hyeyeon Gong <kck85075@naver.com>
2025-11-10 04:29:42 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 116 ms, Memory: 21868 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.kt
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.kt
@@ -1,60 +1,46 @@
+import java.util.StringTokenizer
+import kotlin.math.max
+import kotlin.math.min
+
 fun main() {
     val br = System.`in`.bufferedReader()
     val N = br.readLine().toInt()
+    val tempA = StringTokenizer(br.readLine())
+    val A = IntArray(N) { tempA.nextToken().toInt() }
+    val operators = br.readLine().split(" ").map { it.toInt() }
+    var max = Int.MIN_VALUE
+    var min = Int.MAX_VALUE
     
-    val A = br.readLine().split(" ").map { it.toInt() }.toIntArray()
-    val operatorCount = br.readLine().split(" ").map { it.toInt() }.toIntArray()
-    val operators = ArrayList<Int>()
-    val results = ArrayList<Int>()
-    val currentOperators = ArrayList<Int>()
-
-    for (i in 0 .. 3) {
-        while (operatorCount[i] > 0) {
-            operators.add(i)
-            operatorCount[i] -= 1
+    fun calculate(
+        idx: Int,
+        result: Int,
+        plus: Int,
+        minus: Int,
+        mul: Int,
+        div: Int,
+    ) {
+        if (idx == N) {
+            max = max(max, result)
+            min = min(min, result)
+            return
         }
+        
+        if (plus > 0) calculate(idx + 1, result + A[idx], plus - 1, minus, mul, div)
+        if (minus > 0) calculate(idx + 1, result - A[idx], plus, minus - 1, mul, div)
+        if (mul > 0) calculate(idx + 1, result * A[idx], plus, minus, mul - 1, div)
+        if (div > 0) calculate(idx + 1, result / A[idx], plus, minus, mul, div - 1)
     }
     
-    val visited = BooleanArray(operators.size) { false }
-    backtracking(N, A, visited, operators, results, currentOperators)
-
-    println("${results.maxOrNull()}")
-    println("${results.minOrNull()}")
+    calculate(
+        idx = 1, 
+        result = A[0], 
+        plus = operators[0], 
+        minus = operators[1], 
+        mul = operators[2], 
+        div = operators[3],
+    )
+    
+    println("$max")
+    println("$min")
 }
 
-fun backtracking(
-    n: Int,
-    a: IntArray,
-    visited: BooleanArray,
-    operators: ArrayList<Int>,
-    results: ArrayList<Int>,
-    currentOperators: ArrayList<Int>,
-) {
-    if (currentOperators.size == n - 1) {
-        var result = a[0]
-        
-        for (i in 0 ..< n - 1) {
-            when {
-                currentOperators[i] == 0 -> result += a[i + 1]
-                currentOperators[i] == 1 -> result -= a[i + 1]
-                currentOperators[i] == 2 -> result *= a[i + 1]
-                else -> result /= a[i + 1]
-            }
-        }
-
-        results.add(result)
-        return
-    }
-    
-    for (i in 0 ..< operators.size) {
-        if (visited[i]) continue
-        
-        currentOperators.add(operators[i])
-        visited[i] = true
-        
-        backtracking(n, a, visited, operators, results, currentOperators)
-        
-        currentOperators.removeLast()
-        visited[i] = false
-    }
-}
